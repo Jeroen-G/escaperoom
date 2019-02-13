fed147d39ef21f3f06a411b9b94f1c0ac3aeea11
Jeroen <jeroen@vormkracht10.nl>
2019-02-13 16:31:24 +0100

    wip

--- app/Http/Controllers/WordMatchController.php
+++ app/Http/Controllers/WordMatchController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class WordMatchController extends Controller
 {
     public function index()
     {
-        return view('wordmatch.index');
+        return view('wordmatch.index')->withAnswerIsRight(null);
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->word == env('WORD')) {
+            return view('wordmatch.index')->withAnswerIsRight(true)->withAnswer(env('WORD_ANSWER'));
+        }
+
+        return view('wordmatch.index')->withAnswerIsRight(false);
     }
 }
--- resources/views/wordmatch/index.blade.php
+++ resources/views/wordmatch/index.blade.php
@@ -4,11 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if($answerIsRight === true)
+                <div class="alert alert-dismissible alert-success">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Gelukt!</strong> De volgende hint is {{ $answer }}.
+                </div>
+            @elseif($answerIsRight === false)
+                <div class="alert alert-dismissible alert-danger">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Oh snap!</strong> Het antwoord is niet goed!
+                </div>
+            @endif
             <div class="card text-white bg-secondary mb-3">
-                <div class="card-header">Code</div>
-
+                <div class="card-header">Woord invoeren</div>
                 <div class="card-body">
-                    Test
+                    <form action="{{ route('wordmatch.store') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <input type="text" name="word" id="word" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Proberen</button>
+                    </form>
                 </div>
             </div>
         </div>
